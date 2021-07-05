24dfe3a1bee62e07c8da353fa0c93d183d525107
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-07-05 00:25:01 -0400

    Initial commit

---
+++ CalculatedValue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CalculatedValue
+{
+    ///<summary>Holds a set of instructions to calculate a value of type <typeparamref name="T"/></summary>
+    public class CalculatedValue<T> where T : IEquatable<T>
+    {
+        
+        private readonly T _base;
+        private readonly Layer<T>[] _layers;
+        
+        public ref Layer<T> this[int index] => ref _layers[index];
+
+        ///<summary>Initializes layers and base value</summary>
+        public CalculatedValue(byte layers, T @base){
+            
+           _layers = new Layer<T>[layers];
+           for (var i = 0; i < _layers.Length; i++)
+           {
+               _layers[i] = new Layer<T>();
+           }
+            
+           _base = @base;
+        }
+
+        ///<summary>Runs the base value through all the layers and returns the calculated value</summary>
+        public T Calculate()
+        {
+            var value = _base;
+            //foreach tested fasted
+            foreach (var t in _layers)
+            {
+                //ref tested faster than return
+                t.Calculate(ref value);
+            }
+            return value;
+        }
+    }
+
+    ///<summary>A wrapper for a delegate that modifies a value. Callbacks added to an individual layer should not be dependent on order.</summary>
+    public class Layer<T> where T : IEquatable<T>
+    {
+        public delegate void Sig(ref T value);
+
+        private Sig _del = (ref T value) => { };
+        
+        private T _before;
+        private T _after;
+        private bool _isModified;
+        
+        public void Calculate(ref T value)
+        {
+            if (!_isModified && value.Equals(_before))
+            {
+                value = _after;
+                return;
+            }
+
+            _before = value;
+            _del(ref value);
+            _after = value;
+            _isModified = false;
+        }
+
+        public static Layer<T> operator +(Layer<T> layer, Sig del)
+        {
+            layer._del += del;
+            layer._isModified = true;
+            return layer;
+        }
+
+        public static Layer<T> operator -(Layer<T> layer, Sig del)
+        {
+            layer._del -= del;
+            layer._isModified = true;
+            return layer;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculatedValue
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Simple example
+            var cv = new CalculatedValue<int>(2, 5); // 5
+            cv[0] += (ref int val) => val += 2; // 7
+            cv[0] += (ref int val) => val += 3; // 10
+            cv[1] += (ref int val) => val *= 3; // 30
+            Console.WriteLine(cv.Calculate());
+
+            // Example with random (to demonstrate use case of CalculatedValue<double> vs. double)
+            var cv2 = new CalculatedValue<double>(2, 0f); // 0f
+            var r = new Random();
+            cv2[0] += (ref double val) => val += r.NextDouble() * 5f; // 0f-5f
+            cv2[1] += (ref double val) => val *= 2;                   // 0f-10f
+            Console.WriteLine(cv2.Calculate().ToString("F2"));
+
+            // Example with custom type
+            var cv3 = new CalculatedValue<Example>(2, new Example(5)); // 5
+            cv3[0] += (ref Example val) => val.Value += 2; // 7
+            cv3[0] += (ref Example val) => val.Value += 3; // 10
+            cv3[1] += (ref Example val) => val.Value *= 3; // 30
+            Console.WriteLine(cv3.Calculate().Value);
+        }
+    }
+    
+    internal class Example : IEquatable<Example>
+    {
+        public int Value;
+
+        public Example(int val)
+        {
+            Value = val;
+        }
+
+        public bool Equals(Example other) => !(other is null) && Value == other.Value;
+    }
+}
